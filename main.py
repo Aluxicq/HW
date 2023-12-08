fa9bc7713621a0b580a23d3af153231cb7bf2c8f
Aleksei <alexmamleev1@gmail.com>
2023-12-08 16:28:39 +0500

    change task

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import random
-import random
 
 attempts = 3
 print("Попробуй угадать число от 0 до 5!")
 number = random.randint(0, 5)
-while attempts != 0:
+while True:
 
     user_number = int(input("Введи число: "))
     if user_number == number:
@@ -16,7 +15,3 @@ while attempts != 0:
         attempts -= 1
     if attempts == 0:
         print('Ты проиграл!')
-    if attempts == 1 and number >= 3:
-        print('Даю тебе подсказку - число больше или равно трем')
-    if attempts == 1 and number < 3:
-        print('Даю тебе подсказку - число меньше трех')
